61e306cd2359d829098fec6c9ec78e5441a48bfa
mbala999 <76227219+mbala999@users.noreply.github.com>
2020-12-18 15:55:32 -0500

    Update eqw.f90

--- eqw.f90
+++ eqw.f90
@@ -5,7 +5,7 @@
       !            Pramod Subbareddy    <pksubbar@ncsu.edu>          !
       !                    Last modified: 12 Dec 2020                !
       !==============================================================!
-      ! Cite the below the references when using this code:          !
+      ! Consider citing the below references when using this code.   !
       ! - Kawai & Larsson (2012), Physics of Fluids                  !
       ! - Mettu & Subbareddy (2021), AIAA Journal                    !
       !==============================================================!
